856a42f3edb542741e3efcff6fb1f96acb9dc60a
iaiting <iaiting@aliyun.com>
2019-06-05 19:45:20 +0800

    (*) 优化工程

--- sdk/clang/src/ccrypto/ism4.c
+++ sdk/clang/src/ccrypto/ism4.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "iccrypto.h"
 
-SDK_API int SM4CTR_Encrypt(cuchar *in, uchar *out, size_t len, cuchar key[16], uchar iv[16], uchar ecount_buf[16], unsigned int *num) {
-        printf("Enter: %s", __FUNCTION__);
-        return 0;
-}
-
-SDK_API int SM4CTR_Decrypt(cuchar *in, uchar *out, size_t len, cuchar key[16], uchar iv[16], uchar ecount_buf[16], unsigned int *num) {
-    printf("Enter: %s", __FUNCTION__);
-    return 0;
-}
+//SDK_API int SM4CTR_Encrypt(cuchar *in, uchar *out, size_t len, cuchar key[16], uchar iv[16], uchar ecount_buf[16], unsigned int *num) {
+//        printf("Enter: %s", __FUNCTION__);
+//        return 0;
+//}
+//
+//SDK_API int SM4CTR_Decrypt(cuchar *in, uchar *out, size_t len, cuchar key[16], uchar iv[16], uchar ecount_buf[16], unsigned int *num) {
+//    printf("Enter: %s", __FUNCTION__);
+//    return 0;
+//}
--- sdk/clang/src/ccrypto/test/main.c
+++ sdk/clang/src/ccrypto/test/main.c
@@ -2,7 +2,49 @@
 
 #include "../iccrypto.h"
 
+typedef unsigned char BYTE;
+
+
+
+void f(unsigned char v)
+
+{
+
+	char c = v;
+
+	unsigned char uc = v;
+
+	unsigned int a = c, b = uc;
+
+	int i = c, j = uc;
+
+	printf("----------------\n");
+
+	printf("%%c: %c, %c\n", c, uc);
+
+	printf("%%X: %X, %X\n", c, uc);
+
+	printf("%%u: %u, %u\n", a, b);
+
+	printf("%%d: %d, %d\n", i, j);
+
+}
+
+
 int main(int argc, char *argv[]) {
+
+	
+	f(0x80);
+
+	f(0x7F);
+	
+	unsigned long a = 0x00000000;
+	char b = 0xFF;
+	a |= b; // a = 0x8000007F
+	a = 0x00000000;
+	BYTE c = 0xFF;
+	a |= c; // a = 0x000000FF
+
 
     printf("88888888888888888\n");
     printf("88888888888888888\n");
@@ -14,5 +56,5 @@ int main(int argc, char *argv[]) {
     printf("88888888888888888\n");
     printf("888888888888888886666\n");
 
-    SM4CTR_Encrypt(NULL, NULL, 0, NULL, NULL, NULL, NULL);
+    //SM4CTR_Encrypt(NULL, NULL, 0, NULL, NULL, NULL, NULL);
 }
